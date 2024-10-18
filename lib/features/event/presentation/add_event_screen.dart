2bf5620048122ef9007e60f15b0a9e356298fc4e
Hana Ouerghemmi <5004@holbertonstudents.com>
2024-10-18 17:51:50 +0100

    add items Ui in add event screen

--- lib/features/event/presentation/add_event_screen.dart
+++ lib/features/event/presentation/add_event_screen.dart
@@ -1,8 +1,10 @@
+import 'package:date_keeper/core/core.dart';
+import 'package:date_keeper/features/auth/domain/entities/user_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class AddEventScreen extends StatefulWidget {
-
   const AddEventScreen({super.key});
 
   @override
@@ -36,9 +38,23 @@ class _AddEventScreenState extends State<AddEventScreen> {
     }
   }
 
+  ///*** list user static */
+
+  List<UserEntity> users = [
+    UserEntity(
+        username: 'Alice',
+        profilePictureUrl: 'https://i.pravatar.cc/150?img=1'),
+    UserEntity(
+        username: 'Bob', profilePictureUrl: 'https://i.pravatar.cc/150?img=2'),
+    UserEntity(
+        username: 'Charlie',
+        profilePictureUrl: 'https://i.pravatar.cc/150?img=3'),
+  ];
+  UserEntity? _selectedUser;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Add New Events'),
       ),
@@ -64,7 +80,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
                   },
                 ),
                 SizedBox(height: 16.0),
-        
+
                 // Description Input
                 TextFormField(
                   controller: _descriptionController,
@@ -81,7 +97,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
                   },
                 ),
                 SizedBox(height: 16.0),
-        
+
                 // Date Input (with calendar picker)
                 TextFormField(
                   controller: _dateController,
@@ -102,8 +118,8 @@ class _AddEventScreenState extends State<AddEventScreen> {
                   },
                 ),
                 SizedBox(height: 16.0),
-                // status color 
-        
+                // status color
+
                 Text(
                   'Choose a Color:',
                   style: TextStyle(fontSize: 16.0),
@@ -138,6 +154,40 @@ class _AddEventScreenState extends State<AddEventScreen> {
                     });
                   },
                 ),
+
+                // User Checklist
+                Text(
+                  'Select Users:',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+               
+               DropdownButton<UserEntity>(
+                  value: _selectedUser,
+                  hint: Text('Select a user'),
+                  isExpanded: true,
+                  onChanged: (UserEntity? newValue) {
+                    setState(() {
+                      _selectedUser = newValue;
+                    });
+                  },
+                  items: users.map((UserEntity user) {
+                    return DropdownMenuItem<UserEntity>(
+                      value: user,
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(user.profilePictureUrl!),
+                            radius: 16,
+                          ),
+                          SizedBox(width: 10),
+                          Text(user.username!),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+                smallPaddingVert,
+
                 // Submit Button
                 ElevatedButton(
                   onPressed: () {
@@ -147,13 +197,14 @@ class _AddEventScreenState extends State<AddEventScreen> {
                       final description = _descriptionController.text;
                       final date = _dateController.text;
                       final statusColor = _selectedColor;
-        
+                      final selectedUser = _selectedUser;
+
                       // Print form values for demonstration
                       print('Title: $title');
                       print('Description: $description');
                       print('Date: $date');
                       print('Date: $statusColor');
-        
+
                       // Clear the form after submission
                       _titleController.clear();
                       _descriptionController.clear();
@@ -168,6 +219,5 @@ class _AddEventScreenState extends State<AddEventScreen> {
         ),
       ),
     );
-  
   }
 }
