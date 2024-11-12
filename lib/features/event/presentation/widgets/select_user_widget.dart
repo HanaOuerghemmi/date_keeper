2145612376cbf45f7e6ab2b3959136274ff9d85c
Majdi Aribi <Majdi.Aribi@pass-consulting.com>
2024-11-12 10:51:46 +0100

    [XIV-82] select user in create event and replace 'abc' icon with 'select' icon

--- lib/features/event/presentation/widgets/select_user_widget.dart
+++ lib/features/event/presentation/widgets/select_user_widget.dart
@@ -39,134 +39,113 @@ class _SelectUserWidgetState extends State<SelectUserWidget> {
   Widget build(BuildContext context) {
     log('widget characters');
     return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-      Container(
-        width: MediaQuery.of(context).size.width,
-        height: 105,
-        decoration: BoxDecoration(
-          color: whiteColor,
-          boxShadow: [
-            BoxShadow(
-              color: black.withOpacity(0.2),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(0, 3),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: BlocConsumer<GetAllCharacterCubit, GetAllCharacterState>(
+              listener: (context, state) {
+                state.maybeWhen(
+                  error: () {},
+                  orElse: () {},
+                );
+              },
+              builder: (context, state) {
+                return state.maybeWhen(
+                  initial: () =>
+                      const Center(child: Text("No users available.")),
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                  loaded: (characters) => characters.isEmpty
+                      ? const Center(child: Text("No users available."))
+                      : SizedBox(
+                          height: 150,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: characters.length,
+                            itemBuilder: (context, index) {
+                              final character = characters[index];
+
+                              return GestureDetector(
+                                //! refactor code make just one widget with all thing .....
+                                onTap: () {
+                                  setState(() {
+                                    if (_selectedUser == null ||
+                                        _selectedUser != character) {
+                                      _selectedUser = character;
+                                      widget.onUserSelected(character);
+                                      if (character != null) {
+                                        log('Selected user: ${character.name}');
+                                      } else {
+                                        _selectedUser = null;
+                                        log('No user selected');
+                                      }
+                                    } else if (_selectedUser == character) {
+                                      _selectedUser = null;
+                                      widget.onUserSelected(null);
+                                    }
+                                  });
+                                },
+                                child: Stack(
+                                  alignment: AlignmentDirectional.center,
+                                  children: [
+                                    CharcterItemWidget(
+                                        text: character.name ?? '',
+                                        image: Image.network(
+                                          character.profilePicture!,
+                                          width: 50,
+                                          height: 50,
+                                          fit: BoxFit.cover,
+                                          errorBuilder:
+                                              (context, error, stackTrace) =>
+                                                  Icon(
+                                            Icons.person,
+                                            size: 50 * 0.6,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          setState(() {
+                                            if (_selectedUser == null ||
+                                                _selectedUser != character) {
+                                              _selectedUser = character;
+                                              widget.onUserSelected(character);
+                                              if (character != null) {
+                                                log('Selected user: ${character.name}');
+                                              } else {
+                                                _selectedUser = null;
+                                                log('No user selected');
+                                              }
+                                            } else if (_selectedUser ==
+                                                character) {
+                                              _selectedUser = null;
+                                              widget.onUserSelected(null);
+                                            }
+                                          });
+                                        }),
+                                    if (character == _selectedUser)
+                                      const Center(
+                                        child: Icon(
+                                          Icons.check,
+                                          color: whiteColor,
+                                          size: 70,
+                                          fill: 1,
+                                          weight: 10,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                  orElse: () => Center(child: Text('Unexpected state')),
+                );
+              },
             ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: BlocConsumer<GetAllCharacterCubit, GetAllCharacterState>(
-                  listener: (context, state) {
-                    state.maybeWhen(
-                      error: () {},
-                      orElse: () {},
-                    );
-                  },
-                  builder: (context, state) {
-                    return state.maybeWhen(
-                      initial: () =>
-                          const Center(child: Text("No users available.")),
-                      loading: () =>
-                          const Center(child: CircularProgressIndicator()),
-                      loaded: (characters) => characters.isEmpty
-                          ? const Center(child: Text("No users available."))
-                          : ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: characters.length,
-                              itemBuilder: (context, index) {
-                                final character = characters[index];
-                                //! refactor code ........
-                                return 
-                                // character == _selectedUser
-                                //     ? CharcterItemWidget(
-                                //         text: character.name ?? '',
-                                //         image: Image.network(
-                                //           character.profilePicture!,
-                                //           width: 50,
-                                //           height: 50,
-                                //           fit: BoxFit.cover,
-                                //           errorBuilder:
-                                //               (context, error, stackTrace) =>
-                                //                   Icon(
-                                //             Icons.person,
-                                //             size: 50 * 0.6,
-                                //             color: Colors.grey,
-                                //           ),
-                                //         ),
-                                //         onTap: () {
-                                //           setState(() {
-                                //             _selectedUser = character;
-                                //             widget.onUserSelected(character);
-                                //             if (character != null) {
-                                //               log('Selected user: ${character.name}');
-                                //             } else {
-                                //               log('No user selected');
-                                //             }
-                                //           });
-                                //         })
-                                //     :
-                                     Stack(
-                                        alignment: AlignmentDirectional.topCenter,
-                                        children: [
-                                          CharcterItemWidget(
-                                              text: character.name ?? '',
-                                              image: Image.network(
-                                                character.profilePicture!,
-                                                width: 50,
-                                                height: 50,
-                                                fit: BoxFit.cover,
-                                                errorBuilder: (context, error,
-                                                        stackTrace) =>
-                                                    Icon(
-                                                  Icons.person,
-                                                  size: 50 * 0.6,
-                                                  color: Colors.grey,
-                                                ),
-                                              ),
-                                              onTap: () {
-                                                setState(() {
-                                                  if(_selectedUser == null || _selectedUser != character){
-                                                     _selectedUser = character;
-                                                  widget.onUserSelected(
-                                                      character);
-                                                  if (character != null) {
-                                                    log('Selected user: ${character.name}');
-                                                  } else {
-                                                    _selectedUser =null;
-                                                    log('No user selected');
-                                                  }
-
-                                                  }else if(_selectedUser == character){
-                                                     _selectedUser = null;
-                                                  widget.onUserSelected(
-                                                      null);
-
-                                                  }
-                                                 
-                                                });
-                                              }),
-                                           if (character == _selectedUser)
-                                            Icon(
-                                              Icons.abc,
-                                              color: black,
-                                            ),
-                                        ],
-                                      );
-                              },
-                            ),
-                      orElse: () => Center(child: Text('Unexpected state')),
-                    );
-                  },
-                ),
-              ),
-            ],
           ),
-        ),
+        ],
       ),
     ]);
   }
