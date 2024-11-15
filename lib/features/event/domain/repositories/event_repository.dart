dbbc803ffb704121b82491d38bf3bb8f5a9b03d3
Hana Ouerghemmi <5004@holbertonstudents.com>
2024-11-15 11:15:20 +0100

    change the the getall event future to stream

--- lib/features/event/domain/repositories/event_repository.dart
+++ lib/features/event/domain/repositories/event_repository.dart
@@ -1,10 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:date_keeper/core/error/failures.dart';
-import 'package:date_keeper/features/character/domain/entities/character_entity.dart';
 import 'package:date_keeper/features/event/domain/entities/event_entity.dart';
 
 abstract class EventRepository {
-  Future<Either<Failure, List<EventEntity>>> getAllEvents();
+  Stream<Either<Failure, List<EventEntity>>> getAllEvents();
   Future<Either<Failure, List<EventEntity>>> getAllEventsByCharacter({String idCharacter});
   Future<Either<Failure, EventEntity>> createEvent({EventEntity? event});
   Future<Either<Failure, EventEntity>> updateEvent({EventEntity updatedEvent});
